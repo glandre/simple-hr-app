c39a633b65ef8cbf7f10f79138cdb583815ecb10
Geraldo Landre <geraldo.landre@gmail.com>
2021-08-29 17:51:58 -0700

    chore: Improve deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,33 @@
 # download code
+echo "[deploy.sh] Downloading latest version..."
 cd ~/simple-hr-app && git checkout main
 cd ~/simple-hr-app && git pull
 
 # install dependencies
+echo "[deploy.sh] Installing dependencies..."
 cd ~/simple-hr-app/back-end && composer install
 
 # back up current version
+echo "[deploy.sh] Backup - deleting last.bak..."
+rm -rf ~/backups/back-end/last.bak
+echo "[deploy.sh] Backup - backing up last directory..."
+mv ~/backups/back-end/last ~/backups/back-end/last.bak
+
+echo "[deploy.sh] Backup - creating date-based directory..."
 folder_name=$(date '+%Y-%m-%d-%H-%M-%S')
-mkdir -p ~/backups/back-end/last
 mkdir -p ~/backups/back-end/$folder_name
+
+echo "[deploy.sh] Backup - backing up existing version..."
 sudo mv /var/www/simple-hr-app/back-end ~/backups/back-end/last
 sudo cp -R ~/backups/back-end/last ~/backups/back-end/$folder_name
 
 # copy files to the target folder
+echo "[deploy.sh] Deploying updated version..."
 sudo cp -R ~/simple-hr-app/back-end /var/www/simple-hr-app/back-end
 
 # set up permissions
+echo "[deploy.sh] Setting up permissions..."
 sudo chown -R www-data.www-data /var/www/simple-hr-app/back-end/storage
 sudo chown -R www-data.www-data /var/www/simple-hr-app/back-end/bootstrap/cache
+
+echo "[deploy.sh] Done."
